e4349ca6dc0fe52e8f19ebd65065e53066561f9b
fernando <fernando@Jr>
2015-04-13 10:34:13 -0700

    Se sustituyeron los metódos depreciados

--- src/mx/edu/cobach/persistencia/util/HibernateUtil.java
+++ src/mx/edu/cobach/persistencia/util/HibernateUtil.java
@@ -5,32 +5,32 @@
  */
 package mx.edu.cobach.persistencia.util;
 
-import org.hibernate.cfg.AnnotationConfiguration;
+
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 /**
- * Hibernate Utility class with a convenient method to get Session Factory
- * object.
- *
- * @author Alex
+ *Hibernate Util es una clase con un metodo conveniente para obtener un objeto 
+ *de la fabrica de sesiones
  */
 public class HibernateUtil {
 
-    private static final SessionFactory sessionFactory;
+    private static SessionFactory sessionFactory;
     
-    static {
-        try {
-            // Create the SessionFactory from standard (hibernate.cfg.xml) 
-            // config file.
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            // Log the exception. 
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
+    private static void createSessionFactory(){
+        Configuration config = new Configuration();
+        config.configure();
+        StandardServiceRegistry sr = new StandardServiceRegistryBuilder().
+                applySettings(config.getProperties()).build();
+        sessionFactory = config.buildSessionFactory(sr);
     }
     
     public static SessionFactory getSessionFactory() {
+        if(sessionFactory == null){
+            createSessionFactory(); 
+        }
         return sessionFactory;
     }
 }
