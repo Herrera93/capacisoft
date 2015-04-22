0bfa4dada866a8cc723e03b492c77ee12d660fa1
fernando <fernando@Jr>
2015-04-21 17:17:51 -0700

    Se creao la clase CursoDelegate

---
+++ src/mx/edu/cobach/negocio/delegate/CursoDELEGATE.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.cobach.negocio.delegate;
+
+import java.util.List;
+import mx.edu.cobach.negocio.facade.ServiceLocatorFACADE;
+
+/**
+ *
+ * @author fernando
+ */
+public class CursoDELEGATE {
+    CursoDELEGATE(){
+        
+    }
+    public List<Object> findTipoCurso(int id, Class clazz) {
+        return ServiceLocatorFACADE.getCurso().findTipoCurso(id, clazz);
+    }
+}
