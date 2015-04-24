7502e4b13f5d8b3f42e90aab0708c0a4bbbb98db
liuts <liuts@10.10.1.25>
2015-04-23 20:59:30 -0700

    Se desbloqueo las opciones de proveedor, se agrego un titulo y se agrego un toolkit

--- src/mx/edu/cobach/vista/PnlCursoRealizar.java
+++ src/mx/edu/cobach/vista/PnlCursoRealizar.java
@@ -46,19 +46,23 @@ public class PnlCursoRealizar extends javax.swing.JPanel {
         fechaInicio_G_DCh = new com.toedter.calendar.JDateChooser();
         fechaTerminacion_G_DCh = new com.toedter.calendar.JDateChooser();
         jLabel1 = new javax.swing.JLabel();
+        nota_G_Lbl = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         seleccion_LA_Lbl = new javax.swing.JLabel();
         tipo_LA_CBx = new javax.swing.JComboBox();
         jScrollPane4 = new javax.swing.JScrollPane();
         tablaSeleccion_LA_Tbl = new javax.swing.JTable();
-        agregarEmpleado_LA_Btn = new javax.swing.JButton();
-        agregarTodos_AL_Btn = new javax.swing.JButton();
         jScrollPane5 = new javax.swing.JScrollPane();
         tablaSeleccionCampo_LA_tbl = new javax.swing.JTable();
         guardar_LA_Btn = new javax.swing.JButton();
         SeleccionCampo_LA_TFd = new javax.swing.JTextField();
         seleccionCampo_LA_Lbl = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        nota_LI_Lbl = new javax.swing.JLabel();
+        agregarEmpleado_IPL_Btn = new javax.swing.JButton();
+        agregarTodos_IPL_Btn = new javax.swing.JButton();
+        eliminarEmpleado_OPL_Btn = new javax.swing.JButton();
+        eliminarTodos_IPL_Btn = new javax.swing.JButton();
 
         setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
@@ -112,6 +116,8 @@ public class PnlCursoRealizar extends javax.swing.JPanel {
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel1.setText("Agregar");
 
+        nota_G_Lbl.setText("Ingrese la información a almacenar");
+
         javax.swing.GroupLayout general_PnlLayout = new javax.swing.GroupLayout(general_Pnl);
         general_Pnl.setLayout(general_PnlLayout);
         general_PnlLayout.setHorizontalGroup(
@@ -142,7 +148,8 @@ public class PnlCursoRealizar extends javax.swing.JPanel {
                                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, general_PnlLayout.createSequentialGroup()
                                         .addComponent(nombreCurso_G_Lbl)
                                         .addGap(102, 102, 102)
-                                        .addComponent(nombreCurso_G_TFd, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                        .addComponent(nombreCurso_G_TFd, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addComponent(nota_G_Lbl)))
                         .addGap(59, 59, 59)
                         .addGroup(general_PnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(general_PnlLayout.createSequentialGroup()
@@ -164,7 +171,9 @@ public class PnlCursoRealizar extends javax.swing.JPanel {
             .addGroup(general_PnlLayout.createSequentialGroup()
                 .addGap(38, 38, 38)
                 .addComponent(jLabel1)
-                .addGap(42, 42, 42)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(nota_G_Lbl)
+                .addGap(22, 22, 22)
                 .addGroup(general_PnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(tipo_G_CBx, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(tipo_G_Lbl)
@@ -225,25 +234,19 @@ public class PnlCursoRealizar extends javax.swing.JPanel {
         });
         jScrollPane4.setViewportView(tablaSeleccion_LA_Tbl);
 
-        agregarEmpleado_LA_Btn.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        agregarEmpleado_LA_Btn.setText(">");
-
-        agregarTodos_AL_Btn.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        agregarTodos_AL_Btn.setText(">>");
-
         tablaSeleccionCampo_LA_tbl.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null},
-                {null, null, null},
-                {null, null, null},
-                {null, null, null}
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null}
             },
             new String [] {
-                "Numero del empleado", "Nombre del empleado", "Eliminar"
+                "Numero del empleado", "Nombre del empleado"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false
+                false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
@@ -263,6 +266,28 @@ public class PnlCursoRealizar extends javax.swing.JPanel {
         jLabel2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel2.setText("Agregar");
 
+        nota_LI_Lbl.setText("Ingrese la información a almacenar");
+
+        agregarEmpleado_IPL_Btn.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        agregarEmpleado_IPL_Btn.setText(">");
+        agregarEmpleado_IPL_Btn.setToolTipText("Seleccioné un empleado parala lista de asistencia");
+        agregarEmpleado_IPL_Btn.setEnabled(false);
+
+        agregarTodos_IPL_Btn.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        agregarTodos_IPL_Btn.setText(">>");
+        agregarTodos_IPL_Btn.setToolTipText("Agregar todos los empleados");
+        agregarTodos_IPL_Btn.setEnabled(false);
+
+        eliminarEmpleado_OPL_Btn.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        eliminarEmpleado_OPL_Btn.setText("<");
+        eliminarEmpleado_OPL_Btn.setToolTipText("Seleccioné un empleado a eliminar");
+        eliminarEmpleado_OPL_Btn.setEnabled(false);
+
+        eliminarTodos_IPL_Btn.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        eliminarTodos_IPL_Btn.setText("<<");
+        eliminarTodos_IPL_Btn.setToolTipText("Eliminar todos los empleados");
+        eliminarTodos_IPL_Btn.setEnabled(false);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -274,16 +299,21 @@ public class PnlCursoRealizar extends javax.swing.JPanel {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(38, 38, 38)
-                        .addComponent(jLabel2))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(22, 22, 22)
                         .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 304, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(agregarTodos_AL_Btn)
-                            .addComponent(agregarEmpleado_LA_Btn, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(agregarTodos_IPL_Btn, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(agregarEmpleado_IPL_Btn, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(eliminarEmpleado_OPL_Btn, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(eliminarTodos_IPL_Btn, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(38, 38, 38)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(nota_LI_Lbl)
+                            .addComponent(jLabel2))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(seleccionCampo_LA_Lbl)
@@ -301,27 +331,37 @@ public class PnlCursoRealizar extends javax.swing.JPanel {
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(192, 192, 192)
-                .addComponent(agregarEmpleado_LA_Btn, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(66, 66, 66)
-                .addComponent(agregarTodos_AL_Btn, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 128, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(guardar_LA_Btn, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(45, 45, 45))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(39, 39, 39)
                 .addComponent(jLabel2)
-                .addGap(19, 19, 19)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(nota_LI_Lbl)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(seleccion_LA_Lbl)
                     .addComponent(tipo_LA_CBx, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(seleccionCampo_LA_Lbl)
                     .addComponent(SeleccionCampo_LA_TFd, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(30, 30, 30)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, 272, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(30, 30, 30)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, 272, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(136, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(agregarEmpleado_IPL_Btn, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(25, 25, 25)
+                        .addComponent(agregarTodos_IPL_Btn, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(23, 23, 23)
+                        .addComponent(eliminarEmpleado_OPL_Btn, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(35, 35, 35)
+                        .addComponent(eliminarTodos_IPL_Btn, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(163, 163, 163))))
         );
 
         informacionCurso_TP.addTab("Lista de asistencia", jPanel1);
@@ -345,10 +385,12 @@ public class PnlCursoRealizar extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField SeleccionCampo_LA_TFd;
-    private javax.swing.JButton agregarEmpleado_LA_Btn;
-    private javax.swing.JButton agregarTodos_AL_Btn;
+    private javax.swing.JButton agregarEmpleado_IPL_Btn;
+    private javax.swing.JButton agregarTodos_IPL_Btn;
     private javax.swing.JTextArea descripcionCurso_G_TAa;
     private javax.swing.JLabel descripcion_G_Lbl;
+    private javax.swing.JButton eliminarEmpleado_OPL_Btn;
+    private javax.swing.JButton eliminarTodos_IPL_Btn;
     private javax.swing.JLabel fechaInicial_G_Lbl;
     private com.toedter.calendar.JDateChooser fechaInicio_G_DCh;
     private com.toedter.calendar.JDateChooser fechaTerminacion_G_DCh;
@@ -365,6 +407,8 @@ public class PnlCursoRealizar extends javax.swing.JPanel {
     private javax.swing.JScrollPane jScrollPane5;
     private javax.swing.JLabel nombreCurso_G_Lbl;
     private javax.swing.JTextField nombreCurso_G_TFd;
+    private javax.swing.JLabel nota_G_Lbl;
+    private javax.swing.JLabel nota_LI_Lbl;
     private javax.swing.JComboBox numero_G_CBx;
     private javax.swing.JLabel numero_G_Lbl;
     private javax.swing.JComboBox sede_G_CBx;
