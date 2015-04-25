e764b82a76ec821394a28e8750e517feedb87e18
Alex <Alex@192.168.1.69>
2015-04-25 12:31:30 -0700

    Arreglando el merge

--- src/mx/edu/cobach/persitencia/CursoDAO.java
+++ src/mx/edu/cobach/persitencia/CursoDAO.java
@@ -6,46 +6,18 @@
 package mx.edu.cobach.persitencia;
 
 import java.util.List;
-import mx.edu.cobach.persistencia.entidades.Curso;
-import mx.edu.cobach.persistencia.util.HibernateUtil;
-import org.hibernate.HibernateException;
-import org.hibernate.criterion.Restrictions;
 
 /**
  *
  * @author liuts
  */
-public class CursoDAO extends BaseDAO{
+public class CursoDAO extends BaseDAO {
     
     public List<Object> findNombre(String nombre){
-        List<Object> ts = null;        
-        try{
-            HibernateUtil.openSession();
-            HibernateUtil.beginTransaction();
-            ts = HibernateUtil.getSession().createCriteria(Curso.class)
-                    .add(Restrictions.ilike("nombre", nombre + "%")).list();
-            System.out.println("Buscar por nombre::CURSO");
-        }catch(HibernateException e){
-            HibernateUtil.rollbackTransaction();
-        }finally{
-            HibernateUtil.closeSession();
-        }
-        return ts;
+        return null;
     }
     
     public List<Object> findTipoCurso(int id){
-        List<Object> ts = null;        
-        try{
-            HibernateUtil.openSession();
-            HibernateUtil.beginTransaction();
-            ts = HibernateUtil.getSession().createCriteria(Curso.class)
-                    .add(Restrictions.ilike("tipoCurso.id", id)).list();
-            System.out.println("Buscar por Tipo Curso::CURSO");
-        }catch(HibernateException e){
-            HibernateUtil.rollbackTransaction();
-        }finally{
-            HibernateUtil.closeSession();
-        }
-        return ts;
+        return null;
     }
 }
