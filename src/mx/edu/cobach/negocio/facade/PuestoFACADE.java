53f44bcea396234b2323650ced2fef687ec6b032
fernando <fernando@Jr>
2015-05-04 12:07:19 -0700

    Se extendio la Clase basefacade y se añadio el constructor super, ademas se creo la clase departamento facade

--- src/mx/edu/cobach/negocio/facade/PuestoFACADE.java
+++ src/mx/edu/cobach/negocio/facade/PuestoFACADE.java
@@ -6,13 +6,18 @@
 package mx.edu.cobach.negocio.facade;
 
 import java.util.List;
+import mx.edu.cobach.persitencia.PuestoDAO;
 import mx.edu.cobach.persitencia.ServiceLocator;
 
 /**
  *
  * @author liuts
  */
-public class PuestoFACADE {
+public class PuestoFACADE extends PuestoDAO{
+    public PuestoFACADE(){
+        super();
+    }
+    
     public List<Object> find(String nombre, Class clazz) {
         ServiceLocator.getPuesto().setEntity(clazz);
         return ServiceLocator.getPuesto().find(nombre);
