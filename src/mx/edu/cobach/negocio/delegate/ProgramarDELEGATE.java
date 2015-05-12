2492f7794192ba7eb5f89d9b416b7a8405383a5c
liuts <liuts@liuts-PC>
2015-05-12 11:37:38 -0700

    Se agregaron metodos para encontrar programarCurso

---
+++ src/mx/edu/cobach/negocio/delegate/ProgramarDELEGATE.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.cobach.negocio.delegate;
+
+import java.util.List;
+import mx.edu.cobach.negocio.facade.ServiceLocatorFACADE;
+import mx.edu.cobach.persistencia.entidades.Curso;
+import mx.edu.cobach.persistencia.entidades.Departamento;
+import mx.edu.cobach.persistencia.entidades.Plantel;
+import mx.edu.cobach.persistencia.entidades.Puesto;
+import mx.edu.cobach.persistencia.entidades.Sede;
+
+/**
+ *
+ * @author liuts
+ */
+public class ProgramarDELEGATE extends BaseDELEGATE{
+    
+    public List<Object> findByNombre(String nombre) {
+        return ServiceLocatorFACADE.getEmpleadoFacade().findByNombre(nombre);
+    }
+    public List<Object> findByDepartamento(Departamento departamento) {
+        return ServiceLocatorFACADE.getEmpleadoFacade().findByDepartamento(departamento);
+    }
+    public List<Object> findByPuesto(Puesto puesto) {
+        return ServiceLocatorFACADE.getEmpleadoFacade().findByPuesto(puesto);
+    }
+    public List<Object> findByPlantel(Plantel plantel) {
+        return ServiceLocatorFACADE.getEmpleadoFacade().findByPlantel(plantel);
+    }
+    public List<Object> findEncuesta(Class encuesta) {
+        return ServiceLocatorFACADE.getInstance().findAll(encuesta);
+    }
+    public List<Object> findByCurso(Curso curso) {
+        return ServiceLocatorFACADE.getPrograma().findByCurso(curso);
+    }
+    public List<Object> findBySede(Sede sede) {
+        return ServiceLocatorFACADE.getPrograma().findBySede(sede);
+    }
+}
