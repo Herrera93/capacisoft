e1dc24a9481fb966c07341bc38555b44dd64551c
liuts <liuts@liuts-PC.lan>
2015-05-17 14:36:48 -0700

    Se agrego la documentacion faltante y se modificaron metodos por nombre de curso a evento

--- src/mx/edu/cobach/vista/controlador/ProgramarControlador.java
+++ src/mx/edu/cobach/vista/controlador/ProgramarControlador.java
@@ -8,7 +8,7 @@ package mx.edu.cobach.vista.controlador;
 import java.util.ArrayList;
 import java.util.List;
 import mx.edu.cobach.negocio.delegate.ServiceLocatorDELEGATE;
-import mx.edu.cobach.persistencia.entidades.Curso;
+import mx.edu.cobach.persistencia.entidades.Evento;
 import mx.edu.cobach.persistencia.entidades.Departamento;
 import mx.edu.cobach.persistencia.entidades.Empleado;
 import mx.edu.cobach.persistencia.entidades.EnunciadoLogistica;
@@ -26,24 +26,51 @@ public class ProgramarControlador extends BaseControlador{
     public ProgramarControlador(Comunicador com, Class clazz) {
         super(com, clazz);
     }  
+    /**
+     * Este metodo sirve para buscar el curso implementado a modificar
+     * @param id 
+     */
     public void buscarMod(int id) {
         Object o = ServiceLocatorDELEGATE.getInstance().find(id, clazz);
         com.setInfo(HelperEntidad.descomponerObjeto(o));
     } 
-
-    public void buscarCurId(Integer id, Class<Curso> cursoClase) {
-        Object o = ServiceLocatorDELEGATE.getPrograma().find(id, cursoClase);
+    /**
+     * Sirve para buscar un evento especifico para agregarlo a los cursos a 
+     * implementar
+     * @param id
+     * @param eventoClase 
+     */
+    public void buscarCurId(Integer id, Class<Evento> eventoClase) {
+        Object o = ServiceLocatorDELEGATE.getPrograma().find(id, eventoClase);
         com.setInfo(HelperEntidad.descomponerObjeto(o));
     }
-    
-    public void buscarPorCurso(Curso curso) {
-        List<Object> o = ServiceLocatorDELEGATE.getPrograma().findByCurso(curso);
+    /**
+     * Sirve para buscar todos los eventos que puedan implementarse 
+     * implementar
+     * @param id
+     * @param eventoClase 
+     */
+    public void buscarPorEvento(Evento evento) {
+        List<Object> o = ServiceLocatorDELEGATE.getPrograma().findByEvento(evento);
         com.setTabla(HelperEntidad.descomponerObjetos(o));
     } 
+    /**
+     * Sirve para buscar todos sedes donde se implanto un curso
+     * implementar
+     * @param id
+     * @param eventoClase 
+     */
     public void buscarPorSede(Sede sede) {
         List<Object> o = ServiceLocatorDELEGATE.getPrograma().findBySede(sede);
         com.setTabla(HelperEntidad.descomponerObjetos(o));
     } 
+    /**
+     * Este metodo sirve para buscar el id del empleado y agregarlo a la lista 
+     * de asistencia 
+     * para la lista de asistencia
+     * @param id
+     * @param cursoClase 
+     */
     public void buscarEmpId(Integer id, Class<Empleado> empleado) {
         Object o = ServiceLocatorDELEGATE.getPrograma().find(id, empleado);
         System.out.println(o.toString());
@@ -56,16 +83,19 @@ public class ProgramarControlador extends BaseControlador{
     /**
      * Este metodo sirve para buscar la coincidencia del nombree de los empleados
      * para la lista de asistencia
-     * @param id
-     * @param cursoClase 
+     * @param clave 
      */
     public void buscarEmpN(String clave) {
         List<Object> o = ServiceLocatorDELEGATE.getPrograma().findByNombre(clave);
-        String matriz[][] = HelperEntidad.descomponerObjetos(o);
-        matriz [0][0] = matriz[0][0] + "TLE1"; //se agrega un string para hubicar a que direccion de las 3 tablas se dirigira
+        String matriz[][] = HelperEntidad.descomponerObjetos(o);        
+        //se agrega un string para hubicar a que direccion de las 
+        //3 tablas se dirigira
+        matriz [0][0] = matriz[0][0] + "TLE1";
         com.setTabla(matriz);
     }
-    
+    /**
+     * Este metodo sirve para buscar la encuesta para del curso finalizado
+     */
     public void buscarEncuesta(){
         List<Object> o = ServiceLocatorDELEGATE.getPrograma().findEncuesta(EnunciadoLogistica.class);
         System.out.println(o.size());
