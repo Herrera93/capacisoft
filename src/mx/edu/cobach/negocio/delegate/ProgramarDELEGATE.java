201006c6c3e463dd156e5fa269de533882e910ae
liuts <liuts@liuts-PC.lan>
2015-05-17 14:17:07 -0700

    Se agrego la documentacion necesaria para el entendimiento de los metodos y se cambio el nombre del metodo findByCurso por findByEvento

--- src/mx/edu/cobach/negocio/delegate/ProgramarDELEGATE.java
+++ src/mx/edu/cobach/negocio/delegate/ProgramarDELEGATE.java
@@ -8,7 +8,7 @@ package mx.edu.cobach.negocio.delegate;
 import java.util.Date;
 import java.util.List;
 import mx.edu.cobach.negocio.facade.ServiceLocatorFACADE;
-import mx.edu.cobach.persistencia.entidades.Curso;
+import mx.edu.cobach.persistencia.entidades.Evento;
 import mx.edu.cobach.persistencia.entidades.Departamento;
 import mx.edu.cobach.persistencia.entidades.Plantel;
 import mx.edu.cobach.persistencia.entidades.Puesto;
@@ -19,25 +19,59 @@ import mx.edu.cobach.persistencia.entidades.Sede;
  * @author liuts
  */
 public class ProgramarDELEGATE extends BaseDELEGATE{
-    
+    /**
+     * Obtiene todos los empleados que estan relacionados por el nombre
+     * @param nombre
+     * @return Regresa la lista de los empleados
+     */
     public List<Object> findByNombre(String nombre) {
         return ServiceLocatorFACADE.getEmpleado().findByNombre(nombre);
     }
+    /**
+     * Obtiene todos los empleados que estan relacionados a tal departamento
+     * @param departamento
+     * @return Regresa la lista de los empleados
+     */
     public List<Object> findByDepartamento(Departamento departamento) {
         return ServiceLocatorFACADE.getEmpleado().findByDepartamento(departamento);
     }
+    /**
+     * Obtiene todos los empleados que estan relacionados a tal puesto
+     * @param puesto
+     * @return Regresa la lista de los empleados
+     */
     public List<Object> findByPuesto(Puesto puesto) {
         return ServiceLocatorFACADE.getEmpleado().findByPuesto(puesto);
     }
+    /**
+     * Obtiene todos los empleados que estan relacionados a tal plantel
+     * @param plantel
+     * @return Regresa la lista de los empleados
+     */
     public List<Object> findByPlantel(Plantel plantel) {
         return ServiceLocatorFACADE.getEmpleado().findByPlantel(plantel);
     }
+    /**
+     * Obtiene todas las encuestas de la logistica para el evento.
+     * @param encuesta
+     * @return Regresa la lista de los empleados
+     */
     public List<Object> findEncuesta(Class encuesta) {
         return ServiceLocatorFACADE.getInstance().findAll(encuesta);
     }
-    public List<Object> findByCurso(Curso curso) {
-        return ServiceLocatorFACADE.getPrograma().findByCurso(curso);
+    /**
+     * Obtiene todas las implementaciones relacionadas de un evento dado.
+     * @param evento
+     * @return Regresa la lista de las encuestas
+     */
+    public List<Object> findByEvento(Evento evento) {
+        return ServiceLocatorFACADE.getPrograma().findByEvento(evento);
     }
+    /**
+     * Obtiene todas las implementaciones relacionadas en una sede dada.
+     * @param sede
+     * @return Regresa la lista con las implementaciones
+     */
     public List<Object> findBySede(Sede sede) {
         return ServiceLocatorFACADE.getPrograma().findBySede(sede);
     }
