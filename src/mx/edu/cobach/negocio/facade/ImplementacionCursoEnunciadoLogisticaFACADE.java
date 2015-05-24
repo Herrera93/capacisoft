8397fbae08750f1b89883552f8ff42351d5f1b16
liuts <liuts@10.10.3.132>
2015-05-23 17:39:37 -0700

    Se creo la clase para obtener la calificacion de la logistica y  agrego documentacion

---
+++ src/mx/edu/cobach/negocio/facade/ImplementacionCursoEnunciadoLogisticaFACADE.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.cobach.negocio.facade;
+
+import java.util.List;
+import mx.edu.cobach.persistencia.ServiceLocator;
+import mx.edu.cobach.persistencia.entidades.ImplementacionCurso;
+
+/**
+ *
+ * @author liuts
+ */
+public class ImplementacionCursoEnunciadoLogisticaFACADE{
+    public List<Object> buscarEncuestaPorEvento(ImplementacionCurso eventoProgra) {
+        return ServiceLocator.getEnunciadoCali().buscarEncuestaPorEvento(eventoProgra);
+    }
+}
